1b3bbc83be4db2c7babe5f935225a8a229a098b0
Ganesan4 <Ganesan4@users.noreply.github.com>
2026-08-26 17:38:50 +0530

    remove statusbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,14 @@ import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 import { SalesPopupComponent } from './sales-popup/sales-popup.component';
-import { CommonModule } from '@angular/common';
-import { environment } from '../environments/environment';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, HttpClientModule, ScrollToTopComponent, SalesPopupComponent, CommonModule],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, HttpClientModule, ScrollToTopComponent, SalesPopupComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'GlobalBusinessPage';
-  isStaging = !!(environment as any).isStaging;
 }
